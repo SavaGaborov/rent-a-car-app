4267fd85fc5e6fd5401d06f5f053b455d1146749
savagaborov <savagaborov@gmail.com>
2020-01-08 19:17:19 +0100

    base model

--- mvp/src/main/java/rentacar/mvp/model/BaseModel.java
+++ mvp/src/main/java/rentacar/mvp/model/BaseModel.java
@@ -1,7 +1,40 @@
 package rentacar.mvp.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
 /**
  * Created by savagaborov on 8.1.20..
  */
-public class BaseModel {
+@Getter
+@Setter
+@MappedSuperclass
+@NoArgsConstructor
+public abstract class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_timestamp")
+    private ZonedDateTime createTimestamp;
+
+    @UpdateTimestamp
+    @Column(name = "update_timestamp")
+    private ZonedDateTime updateTimestamp;
+
+    @Column(name = "deleted")
+    private Boolean deleted;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.deleted = false;
+    }
 }
